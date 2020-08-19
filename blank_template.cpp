fa0ddae0aadbbbff61b75b1d5727e1b59b97da7f
HawRon <haowron@gmail.com>
2020-08-19 22:55:50 +0900

    change template

--- blank_template.cpp
+++ blank_template.cpp
@@ -8,22 +8,25 @@
 #include <map>
 #include <stack>
 #include <queue>
+#include <cstdlib>
+#include <unordered_map>
 
 #define rep(i, n) for (int i = 0; i < n; ++i)
+#define repr(e, i) for (auto e: i)
 #define all(x) x.begin(), x.end()
 
 
 using namespace std;
 const int MAXN = 10e5 + 5;
-const int INF = 0x7f7f7f7f;
+const int INF = 0x3f3f3f3f;
 typedef long long ll;
 
 
-
 int main() {
-#ifndef ONLINE_JUDGE
+#ifdef AZUKI_LOCAL
     freopen("../input.txt", "r", stdin);
-    //freopen("../output.txt", "w", stdout);
+    freopen("../output.txt", "w", stdout);
 #endif
+
     return 0;
 }
