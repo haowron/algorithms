4ff38d8e32d2d069dfaa78cba436ecfc2870d538
HawRon <haowron@gmail.com>
2020-09-08 17:43:48 +0900

    atcoder bg172 a-d

---
+++ atcoder/bg172b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cstdlib>
+
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repr(e, i) for (auto e: i)
+#define all(x) x.begin(), x.end()
+
+
+using namespace std;
+const int maxn = 105;
+const int inf = 0x3f3f3f3f;
+typedef long long ll;
+
+
+int main() {
+#ifdef AZUKI_LOCAL
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
+
+    return 0;
+}
